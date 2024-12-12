d1ec266a063f867990a18c427ff7f70a6843ea4e
aominekid <adkid>
2024-12-12 15:20:41 +0100

    add subnet setting

--- main.tf
+++ main.tf
@@ -7,10 +7,12 @@ terraform {
   }
 }
 
+# Provider konfigurieren
 provider "aws" {
-  region = "us-west-2"
+  region = "us-west-2"  # Stelle sicher, dass die Region passt
 }
 
+# VPC erstellen
 resource "aws_vpc" "vpc_docker" {
   cidr_block = "10.0.0.0/27"
   tags = {
@@ -18,15 +20,18 @@ resource "aws_vpc" "vpc_docker" {
   }
 }
 
+# Subnetz erstellen mit Public-IP-Aktivierung
 resource "aws_subnet" "docker_subnet" {
-  vpc_id            = aws_vpc.vpc_docker.id
-  cidr_block        = "10.0.0.0/28"
-  availability_zone = "us-west-2a"
+  vpc_id                     = aws_vpc.vpc_docker.id
+  cidr_block                 = "10.0.0.0/28"
+  availability_zone          = "us-west-2a"
+  map_public_ip_on_launch    = true  # Automatische Public IP aktivieren
   tags = {
     Name = "docker-subnet"
   }
 }
 
+# Internet-Gateway erstellen
 resource "aws_internet_gateway" "igw_docker" {
   vpc_id = aws_vpc.vpc_docker.id
   tags = {
@@ -34,6 +39,7 @@ resource "aws_internet_gateway" "igw_docker" {
   }
 }
 
+# Route Table erstellen und Route hinzufügen
 resource "aws_route_table" "rt_docker" {
   vpc_id = aws_vpc.vpc_docker.id
 
@@ -47,17 +53,20 @@ resource "aws_route_table" "rt_docker" {
   }
 }
 
+# Route Table mit Subnetz verknüpfen
 resource "aws_route_table_association" "rta_docker" {
   subnet_id      = aws_subnet.docker_subnet.id
   route_table_id = aws_route_table.rt_docker.id
 }
 
+# Security Group erstellen
 resource "aws_security_group" "docker_sg" {
   vpc_id = aws_vpc.vpc_docker.id
   tags = {
     Name = "docker-sg"
   }
 
+  # SSH-Zugriff erlauben
   ingress {
     from_port   = 22
     to_port     = 22
@@ -65,6 +74,7 @@ resource "aws_security_group" "docker_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP-Zugriff erlauben
   ingress {
     from_port   = 80
     to_port     = 80
@@ -72,13 +82,15 @@ resource "aws_security_group" "docker_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Custom TCP Port 9002 erlauben
   ingress {
-    from_port   = 9000
-    to_port     = 9001
+    from_port   = 9002
+    to_port     = 9002
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Alle ausgehenden Verbindungen erlauben
   egress {
     from_port   = 0
     to_port     = 0
@@ -87,12 +99,13 @@ resource "aws_security_group" "docker_sg" {
   }
 }
 
+# EC2-Instanz erstellen
 resource "aws_instance" "docker_instance" {
-  ami           = "ami-061dd8b45bc7deb3d"
+  ami           = "ami-061dd8b45bc7deb3d" # Amazon Linux 2 AMI
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.docker_subnet.id
   vpc_security_group_ids = [aws_security_group.docker_sg.id]
-  key_name      = "vockey"
+  key_name      = "vockey"  # Stelle sicher, dass der Key-Pair existiert
 
   user_data = <<-EOF
               #!/bin/bash
@@ -102,7 +115,7 @@ resource "aws_instance" "docker_instance" {
               sudo systemctl start docker
               sudo usermod -aG docker ec2-user
               docker pull minio/minio
-              docker run -d -p 9000:9000 -p 9001:9001 --name minio \
+              docker run -dp 9000:9000 -p 9001:9001 --name minio \
                 -e "MINIO_ROOT_USER=admin" \
                 -e "MINIO_ROOT_PASSWORD=password" \
                 minio/minio server /data --console-address ":9001"
