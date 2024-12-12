669af5a0490f6a7e70c628ae48ea93282b4d78ff
aominekid <adkid>
2024-12-12 14:59:34 +0100

    Added main.tf for wedding-photo-upload project

---
+++ main.tf
@@ -0,0 +1,114 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.55.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_vpc" "vpc_docker" {
+  cidr_block = "10.0.0.0/27"
+  tags = {
+    Name = "vpc-docker"
+  }
+}
+
+resource "aws_subnet" "docker_subnet" {
+  vpc_id            = aws_vpc.vpc_docker.id
+  cidr_block        = "10.0.0.0/28"
+  availability_zone = "us-west-2a"
+  tags = {
+    Name = "docker-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "igw_docker" {
+  vpc_id = aws_vpc.vpc_docker.id
+  tags = {
+    Name = "igw-docker"
+  }
+}
+
+resource "aws_route_table" "rt_docker" {
+  vpc_id = aws_vpc.vpc_docker.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_docker.id
+  }
+
+  tags = {
+    Name = "docker-route-table"
+  }
+}
+
+resource "aws_route_table_association" "rta_docker" {
+  subnet_id      = aws_subnet.docker_subnet.id
+  route_table_id = aws_route_table.rt_docker.id
+}
+
+resource "aws_security_group" "docker_sg" {
+  vpc_id = aws_vpc.vpc_docker.id
+  tags = {
+    Name = "docker-sg"
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 9000
+    to_port     = 9001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "docker_instance" {
+  ami           = "ami-061dd8b45bc7deb3d"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.docker_subnet.id
+  vpc_security_group_ids = [aws_security_group.docker_sg.id]
+  key_name      = "vockey"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo yum install -y httpd
+              sudo systemctl start httpd
+              sudo yum install -y docker
+              sudo systemctl start docker
+              sudo usermod -aG docker ec2-user
+              docker pull minio/minio
+              docker run -d -p 9000:9000 -p 9001:9001 --name minio \
+                -e "MINIO_ROOT_USER=admin" \
+                -e "MINIO_ROOT_PASSWORD=password" \
+                minio/minio server /data --console-address ":9001"
+              EOF
+
+  tags = {
+    Name = "docker-minio"
+  }
+}
